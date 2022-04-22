c131d899bb86b8ba87cd9a461b2c9e8951ce0dd5
Jiayin Liu <liu00366@algonquinlive.com>
2022-04-22 11:51:39 -0400

    Completes the assignment.

--- script.js
+++ script.js
@@ -1,22 +1,110 @@
-const url = 'https://api.nasa.gov/planetary/apod?api_key='
-const api_key = 'xnJ8AOdyLqIzGhq5Q35UOngb5a2NkyzhDvjqlPWI'
+const $btnGet = document.getElementById('btn-get')
+const $result = document.getElementById('result')
+const $date = document.getElementById('date')
+const $hdImage = document.getElementById('hdimage')
+const $favList = document.getElementById('fav_list')
+const $delete = document.getElementById('delete')
 
-const fetchNASAData = async () => {
-  try {
-    const response = await fetch(`${url}${api_key}`)
-    const data = await response.json()
-    console.log('NASA APOD data', data)
-    displayData(data)
-  } catch (error) {
-    console.log(error)
-  }
+
+$btnGet.addEventListener('click', function (e) { 
+    e.preventDefault()
+
+    if($date.value == "") {
+        alert('no date selected!')
+    } else {
+        fetch(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=${$date.value}&end_date=${$date.value}`)
+            
+        .then(response => response.json())
+        .then(json => { 
+            
+            const data = json[0]
+            console.log(data)
+
+            //title, date, expl.
+            $result.innerHTML = `
+            <img id="resultImage" src=${data.url}>
+            <div>
+            <h3>${data.title}</h3>
+            <p class="date">${data.date}</p>
+            <p>${data.explanation}</p>
+            <button id="save">Save to Favourites</button>
+            </div>
+            `
+            function addHiddenClass(e) { 
+                e.classList.add('hidden');
+            }
+
+            function removeHiddenClass(e) { 
+                e.classList.remove('hidden');
+            }
+
+            const $resultImage = document.getElementById('resultImage')
+
+            $resultImage.addEventListener('click', function () {
+                removeHiddenClass($hdImage)   
+                $hdImage.innerHTML=`<img src=${data.hdurl}>`
+            })
+
+            $hdImage.addEventListener('click', function () {
+                addHiddenClass($hdImage)
+            })
+
+            const $save = document.getElementById('save')
+            $save.addEventListener('click', function () {
+                favId++
+                let fav = {
+                    id : favId,
+                    title: data.title,
+                    date: data.date,
+                }
+                display(fav)
+                localStorage.setItem(favId, JSON.stringify(fav))
+                localStorage.setItem('favId', favId)
+            })//end save
+
+        })
+        .catch(error => {
+            alert(`${error.name} - ${error.message}`)
+        })
+    }
+    
+})
+
+
+let favId = 0
+
+        function init() { 
+            if (localStorage.getItem('favId') != null) {
+                for (let i = 0; i <= localStorage.getItem('favId'); i++) {
+                    let fav = localStorage.getItem(i)
+                    if (fav != null) display(JSON.parse(fav))
+                    favId = i
+                    }
+            } else localStorage.setItem('favId', favId)
 }
+        
+// delete a favourites
+ $favList.addEventListener('click', function (e) {
+    if (e.target.classList.contains('delete')) {
+        const $parent = e.target.parentElement
+        //let id = $parent.querySelector('.id').textContent
+        
+        localStorage.removeItem(favId)
+        
+        $parent.remove()
+    }
+})
 
-const displayData = data => {
-  document.getElementById('title').textContent = data.title
-  document.getElementById('date').textContent = data.date
-  document.getElementById('picture').src = data.hdurl
-  document.getElementById('explanation').textContent = data.explanation
-}
-
-fetchNASAData()
+    function display(fav) {
+        $favList.innerHTML += `
+        <li class="fav_element" id="fav_element">
+                <img id="resultImage" src= ${fav.url}>
+                     <div>
+                         <h4>${fav.title}</h4>
+                         <p class="date">${fav.date}</p>
+                         <button class="delete" id="delete" data-delete="${favId}">Delete</button>
+                     </div>
+             </li>`
+    }
+    
+    init() 
